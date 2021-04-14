dcf63bd34604fd65e451c9b3e568408834b9585f
Raul122 <victor.huaycho@tecsup.edu.pe>
2021-04-13 20:34:02 -0500

    second commit

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,10 +2,14 @@ import Content from "./Content"
 import Header from "./Header"
 
 const Course = (props) => {
+    const parts = props.course.parts
+    const total = parts.reduce((s, {exercises}) => s + exercises,0 )
+    console.log(total)
     return (
         <div>
             <Header titulo={props.course.name}/>
             <Content parts={props.course.parts}/>
+            <p>Total de los Exercises: {total}</p>
         </div>
     )
 }
